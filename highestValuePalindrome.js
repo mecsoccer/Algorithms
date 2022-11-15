1cc523c014414a914e4db222c9fe42908fcc039c
jachimma onyenze <jachimma.onyenze@ctw.bmwgroup.com>
2022-11-15 08:46:23 +0000

    update highestValuePalindrome.js

--- highestValuePalindrome.js
+++ highestValuePalindrome.js
@@ -1,11 +1,11 @@
 // https://www.hackerrank.com/challenges/richie-rich/problem?isFullScreen=true
 
 function highestValuePalindrome(s, n, k) {
-    if (n === 1) return k == 1 ? '9' : '-1'
+    if (n === 1) return k > 0 ? '9' : '-1'
             
     let arr = [...s]
     let c = Array(n).fill(0)
-    let x = Math.floor(n/2) + (n&1)
+    let x = parseInt((n + 1)/2)
     
     for (let i = 0; i < x; i++) {
         if (arr[i] !== arr[n-1-i]) {
@@ -25,60 +25,6 @@ function highestValuePalindrome(s, n, k) {
             }
         }
     }
-    return arr.join('')
-}
 
-function highestValuePalindrome1(s, n, k) {
-    // Write your code here
-    //const pal = checkIfPalindrome(s)
-    let i = 0
-    let j = n - 1
-    const itr = parseInt(n/2)
-    let str = s.split('')
-    
-    while (i <= itr) {
-        if (str[i] === str[j]) {
-            i++
-            j--
-            continue
-        } else {
-            if (k > 1 && str[i] !== '9' && str[j] !== '9') {
-                str[i] = str[j] = '9'
-                k -= 2
-                continue
-            }
-            if (str[i] > str[j]) {
-                str[j] = str[i]
-                k--
-            } else {
-                str[i] = str[j]
-                k--
-            }
-        }
-        if (k < 0) return '-1'
-        i++
-        j--
-    }
-    i = 0
-    j = n - 1
-    while (i <= itr) {
-        if (i === j && k > 0) {
-            if (str[i] !== '9') {
-                str[i] = 9
-                k--
-                break
-            }
-        }
-        if (str[i] !== '9' && k >= 2) {
-            str[i] = str[j] = '9'
-            k -= 2
-            i++; j--;
-            continue
-        }
-        if (i === j || k === 0) break
-        i++
-        j--
-    }
-    
-    return str.join('')
+    return arr.join('')
 }
